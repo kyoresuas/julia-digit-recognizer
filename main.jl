52110f915fe435fe06016bfcd344aaf605531634
kyoresuas <stercusasq@gmail.com>
2024-10-19 07:42:28 +0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,1 +1,18 @@
-println("Hello, world!")
+include("model.jl")
+include("train.jl")
+include("data.jl")
+include("utils.jl")
+
+config = Dict(
+    :epochs => 20,
+    :batch_size => 64,
+    :learning_rate => 0.001
+)
+
+train_X, train_y, test_X, test_y = load_data()
+
+model = build_model()
+
+train!(model, train_X, train_y, config)
+
+println("Точность на тестовом наборе: ", accuracy(model, test_X, test_y))
