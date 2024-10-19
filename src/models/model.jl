b9fc6312f396e80fc7b5cb79a2a8ac07680e6d60
kyoresuas <stercusasq@gmail.com>
2024-10-19 09:34:51 +0300

    Добавлено больше слоёв и более продвинутые функции активации

--- src/models/model.jl
+++ src/models/model.jl
@@ -2,9 +2,12 @@ using Flux
 
 function build_model()
     return Chain(
-        Dense(784, 128, relu),  # Первый слой: 784 входа (28x28 пикселей), 128 нейронов
-        Dense(128, 64, relu),   # Второй слой: 128 входов, 64 нейрона
-        Dense(64, 10),          # Выходной слой: 64 входа, 10 выходов (для классов 0-9)
-        softmax                 # Softmax для классификации
+        Dense(784, 512, relu),   # Увеличенный первый слой
+        Dropout(0.5),            # Dropout для регуляризации
+        Dense(512, 256, relu),   # Второй слой
+        Dropout(0.5),            # Еще один Dropout
+        Dense(256, 128, elu),    # Третий слой с ELU
+        Dense(128, 10),          # Выходной слой
+        softmax                  # Softmax для классификации
     )
 end
