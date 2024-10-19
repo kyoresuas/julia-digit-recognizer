b71c4d090b90d1f50e7e41f4d8f4d79a073a5d5a
kyoresuas <stercusasq@gmail.com>
2024-10-19 08:15:57 +0300

    исправлен расчет точности с использованием onecold для меток и предсказаний

--- utils.jl
+++ utils.jl
@@ -1,6 +1,9 @@
 using Flux
+using Statistics
 
 function accuracy(model, test_X, test_y)
-    predictions = Flux.onecold(model(test_X), 0:9)
-    return mean(predictions .== test_y)
+    predictions = Flux.onecold(model(test_X), 0:9)  # Преобразуем предсказания в классы
+    true_labels = Flux.onecold(test_y, 0:9)         # Преобразуем one-hot метки в классы
+    
+    return mean(predictions .== true_labels)
 end
